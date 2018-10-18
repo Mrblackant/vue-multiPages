e684ccea74257e09d75d8821881440df1bbd79ab
赵云 <zzlan@shinyway.com.cn>
2018-10-18 13:49:49 +0800

    加上了是否用自己单独创建实例的方法

--- template/config.js
+++ template/config.js
@@ -1,17 +1,16 @@
 import Vue from 'vue';
-Vue.config.productionTip = false
-import router from '../src/router'
 let baseMounted = localStorage.getItem('baseUrl')
 console.log(baseMounted)
-var App = resolve => require.ensure([], () => resolve(require("../src/views/" + baseMounted + ".vue")));
-
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+// 如果你想用自己创建的实例，在自己单独的js文件里加上：let baseUrl="noNeed" / localStorage.setItem('baseUrl',baseUrl)
+if (baseMounted !== 'noNeed') {
+  var App = resolve => require.ensure([], () => resolve(require("../src/views/" + baseMounted + ".vue")));
+  new Vue({
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+  })
+}
