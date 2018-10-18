e4948a53f31b6f0032bef83a0b09dd4b1b8ed397
赵云 <zzlan@shinyway.com.cn>
2018-10-18 14:02:28 +0800

    加上了是否组价自身引用实例的方法

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,2 +1,15 @@
-let baseUrl="index/index"
-localStorage.setItem('baseUrl',baseUrl)
+// let baseUrl="index/index"
+// localStorage.setItem('baseUrl',baseUrl)
+let baseUrl = "noNeed"
+localStorage.setItem('baseUrl', baseUrl)
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './index.vue'
+
+Vue.use(ElementUI);
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
--- template/config.js
+++ template/config.js
@@ -1,16 +1,19 @@
-import Vue from 'vue';
+// 如果你想用自己创建的实例，在自己单独的js文件里加上：let baseUrl="noNeed" / localStorage.setItem('baseUrl',baseUrl)
 let baseMounted = localStorage.getItem('baseUrl')
 console.log(baseMounted)
+
+if (baseMounted === 'noNeed') {
+  // 组件自己创建了实，将不会再引入下边的资源
+  throw new Error("当前组件自己创建了实例,不会引用公用方法、实例");
+}
+import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
-
-// 如果你想用自己创建的实例，在自己单独的js文件里加上：let baseUrl="noNeed" / localStorage.setItem('baseUrl',baseUrl)
-if (baseMounted !== 'noNeed') {
-  var App = resolve => require.ensure([], () => resolve(require("../src/views/" + baseMounted + ".vue")));
-  new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: { App }
-  })
-}
+// 动态捕获要挂载实例的.vue文件的路径
+var App = resolve => require.ensure([], () => resolve(require("../src/views/" + baseMounted + ".vue")));
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
